b75b189f59fcccc82c1c5a27682e0a02f28ce089
Sam <88316552+samkindev@users.noreply.github.com>
2024-05-15 21:13:02 +0100

    Changed name

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -20,7 +20,7 @@ export const UserMenu = () => {
       >
         <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
           <div className="font-medium truncate">user@nep.com</div>
-          <div>User Name</div>
+          <div>Mervy User</div>
         </div>
         <ul
           className="py-2 text-sm text-gray-700 dark:text-gray-200"
